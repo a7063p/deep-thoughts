250878d374da4cf38f33bbb8ff3c3ec0cafbc387
a7063p <andrewpennucci@yahoo.com>
2021-05-04 16:50:44 -0700

    update tokens

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,7 @@
-// import graphql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-type Thought {
-_id: ID
-thoughtText: String
-createdAt: String
-username: String
-reactionCount: Int
-reactions: [Reaction]
-}
-
-type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-}
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
@@ -28,12 +10,42 @@ type User {
     friends: [User]
   }
 
-type Query {
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
-}`;
+  }
 
-// export the typeDefs
-module.exports = typeDefs
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
